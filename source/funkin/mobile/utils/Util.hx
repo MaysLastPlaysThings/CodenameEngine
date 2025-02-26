a1b12213a5f2ee49a6de729cc704206f9ab7a1de
MaysLastPlay <109207233+MaysLastPlay@users.noreply.github.com>
2025-02-26 20:50:42 +0200

    yea?

--- source/funkin/mobile/utils/Util.hx
+++ source/funkin/mobile/utils/Util.hx
@@ -14,34 +14,25 @@ import lime.app.Application;
 import sys.FileSystem;
 
 class Util {
-   var currentDirectory:String = getStorage.external;
-   var path:String = '';
+   public static var currentDirectory:String = external;
+   public static var path:String = '';
+   public static var external:String = Environment.getExternalStorageDirectory() + '/' + Application.current.meta.get('file') + '/';
+   public static var data = Context.getExternalFilesDir(null) + '/';
+   public static var no_storage:String = System.applicationStorageDirectory;
 
   public static function getMobileStorage() {
-   getStorage(currentDirectory);
    path = Path.addTrailingSlash(currentDirectory);
-  }
-
-  public static function getStorage() {
-  var external:String = Environment.getExternalStorageDirectory() + '/' + Application.current.meta.get('file') + '/';
-  var data = Context.getExternalFilesDir(null) + '/';
-  var no_storage:String = System.applicationStorageDirectory;
   }
 
     public static function getPermissions()
     {
        if(VERSION.SDK_INT >= 33){
-         perms.push("android.permission.READ_MEDIA_VIDEO");
-		     perms.push("android.permission.READ_MEDIA_IMAGES");
-         perms.push("android.permission.READ_MEDIA_AUDIO");
-         Settings.requestSetting('MANAGE_APP_ALL_FILES_ACCESS_PERMISSION');
+		AndroidPermissions.requestPermissions(['READ_MEDIA_IMAGES', 'READ_MEDIA_VIDEO', 'READ_MEDIA_AUDIO']);
+		AndroidSettings.requestSetting('REQUEST_MANAGE_MEDIA');
+	    AndroidSettings.requestSetting('MANAGE_APP_ALL_FILES_ACCESS_PERMISSION');
       } else {
-          perms.push("android.permission.WRITE_EXTERNAL_STORAGE");
-          perms.push("android.permission.READ_EXTERNAL_STORAGE");
-       }
-
-        for (idklmao in perms)
-            Permissions.requestPermissions(idklmao);
+        AndroidPermissions.requestPermissions(['READ_EXTERNAL_STORAGE', 'WRITE_EXTERNAL_STORAGE']);
+	  }
 
     try {
       if(!FileSystem.exists(path))
@@ -49,7 +40,7 @@ class Util {
      } catch (e:Dynamic) {
     trace(e);
     Application.current.window.alert("Seems like you use No Storage Mode.\n If you want to use other modes, check options!", 'Uncaught Error');
-    currentDirectory = getStorage.no_storage;
+    currentDirectory = no_storage;
      path = Path.addTrailingSlash(currentDirectory);
       FileSystem.createDirectory(path);
     }
