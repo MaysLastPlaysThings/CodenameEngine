cf3df72025f96b58b35aa46de8959bf3c60de6c9
MaysLastPlay <109207233+MaysLastPlay@users.noreply.github.com>
2025-02-27 14:25:08 +0200

    Maybe a fix for thing popping up everytime?

--- source/funkin/mobile/utils/Util.hx
+++ source/funkin/mobile/utils/Util.hx
@@ -17,19 +17,21 @@ class Util {
   public static var currentDirectory:String = null;
   public static var path:String = '';
 
-  public static function getMobileDirectory() {
+  public static function getMobileDirectory():String {
    currentDirectory = Environment.getExternalStorageDirectory() + '/.' + Application.current.meta.get('file');
   return currentDirectory;
   }
 
-    public static function getMobilePermissions()
+    public static function getMobilePermissions():Void
     {
     path = Path.addTrailingSlash(Environment.getExternalStorageDirectory() + '/.' + Application.current.meta.get('file'));
   
        if(VERSION.SDK_INT >= 33){
 		Permissions.requestPermissions(['READ_MEDIA_IMAGES', 'READ_MEDIA_VIDEO', 'READ_MEDIA_AUDIO']);
-	   	Settings.requestSetting('REQUEST_MANAGE_MEDIA');
+	    if (!Environment.isExternalStorageManager()) {
+	    Settings.requestSetting('REQUEST_MANAGE_MEDIA');
 	    Settings.requestSetting('MANAGE_APP_ALL_FILES_ACCESS_PERMISSION');
+	    }
       } else {
         Permissions.requestPermissions(['READ_EXTERNAL_STORAGE', 'WRITE_EXTERNAL_STORAGE']);
 	  }
