d16206867b6295d4506bed920b48cd9b82b2b33b
MaysLastPlay <109207233+MaysLastPlay@users.noreply.github.com>
2025-03-11 13:35:36 +0000

    idk, thing doesn't seem to work, probably will need to get logs

--- source/funkin/backend/assets/ScriptedAssetLibrary.hx
+++ source/funkin/backend/assets/ScriptedAssetLibrary.hx
@@ -22,7 +22,7 @@ class ScriptedAssetLibrary extends ModsFolderLibrary {
 	private static var nullValue:Dynamic = {};
 
 	public function new(scriptName:String, args:Array<Dynamic> = null, folderPath:String, libName:String="assets", ?modName:String) {
-		if (folderPath == '') folderPath = #if desktop "./assets/" #else Sys.getCwd() + "assets/" #end;
+		if (folderPath == '') folderPath = #if desktop "./assets/" #else "assets/" #end;
 		if(modName == null) modName = scriptName;
 		super(folderPath, libName, modName);
 		this.scriptName = scriptName;
